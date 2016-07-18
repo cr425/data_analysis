9ad0bdd0fcf8ee83dc5c2fa681876672dfb94edc
Crystal Ramjattan <cr425@cornell.edu>
2016-07-17 21:12:03 -0400

    new picture and desc

---
+++ employee_length_vs_loan_amt.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('LoanStats3a.csv')
+index = ['< 1 year', '1 year', '2 years', '3 years', '4 years', 
+'5 years', '6 years', '7 years', '8 years', '9 years','10 years','10+ years']
+
+mapping = pd.Series([0,1,2,3,4,5,6,7,8,9,10,11], index)
+data['emp_length_int'] = data['emp_length'].map(mapping)
+test_emp_len_int = data['emp_length_int']
+
+x = test_emp_len_int
+y = data['loan_amnt']
+plt.xticks(x, index)
+
+plt.hist(test_emp_len_int.dropna(), color='blue')
+plt.autoscale(tight=True)
+plt.xticks(rotation=45)
+plt.show()
+
+fig = plt.gcf()
+fig.set_size_inches(10, 10, forward=True)
+fig.savefig('test.png', dpi=100)
